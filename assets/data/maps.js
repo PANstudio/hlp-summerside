7dcd5338f1471f9862f3f4dfcbeb0cbf3491a48d
Gabriel <gd.olivar@gmail.com>
2019-07-19 14:34:01 +0100

    Added Mapbox

---
+++ assets/data/maps.js
@@ -0,0 +1,215 @@
+const markers = [
+    {
+        content :"Squirrel Bridge",
+        coords:{
+            lat:46.3954208,
+            lng: -63.8247572
+        },
+    },
+    {
+        content :"Green's Shore Shelter",
+        coords:{
+            lat:46.395279,
+            lng: -63.8045548
+        },
+    },
+    {
+        content :"Canadian Flag",
+        coords:{
+            lat:46.3984425,
+            lng: -63.8012048
+        },
+    },
+    {
+        content :"Sponsorship Vehicle",
+        coords:{
+            lat:46.3983685,
+            lng: -63.8005342
+        },
+    },
+    {
+        content :"Credit Union Place",
+        coords:{
+            lat:46.3988771,
+            lng: -63.800323
+        },
+    },
+    {
+        content :"Shipyard Shelter",
+        coords:{
+            lat:46.3933117,
+            lng:-63.7963203,
+        },
+    },
+    {
+        content :"Vault Door",
+        coords:{
+            lat:46.3918063,
+            lng:-63.7932356,
+        },
+    },
+    {
+        content :"Journal Pioneer Building",
+        coords:{
+            lat:46.3918525,
+            lng:-63.7930854,
+        },
+    },
+    {
+        content :"Menu",
+        coords:{
+            lat:46.3928173,
+            lng:-63.7925798,
+        },
+    },
+    {
+        content :"Inspire Learning Centre",
+        coords:{
+            lat:46.3935443,
+            lng:-63.7919522,
+        },
+    },
+    {
+        content :"Lucky Chairs",
+        coords:{
+            lat:46.393006,
+            lng:-63.7915632,
+        },
+    },
+    {
+        content :"Sundial",
+        coords:{
+            lat:46.3929061,
+            lng:-63.7912092,
+        },
+    },
+    {
+        content :"City Hall",
+        coords:{
+            lat:46.3931484,
+            lng:-63.7905628,
+        },
+    },
+    {
+        content :"Heather Moyse Gazebo",
+        coords:{
+            lat:46.390787,
+            lng:-63.7918035,
+        },
+    },
+    {
+        content :"Sign Box",
+        coords:{
+            lat:46.3916084,
+            lng:-63.7914682,
+        },
+    },
+    {
+        content :"Traffic Light",
+        coords:{
+            lat:46.3917212,
+            lng:-63.7912938,
+        },
+    },
+    {
+        content :"Murals",
+        coords:{
+            lat:46.3926911,
+            lng:-63.7903907,
+        },
+    },
+    {
+        content :"Downtown",
+        coords:{
+            lat:46.3914465,
+            lng:-63.7900923,
+        },
+    },
+    {
+        content :"Lamp Post",
+        coords:{
+            lat:46.3913105,
+            lng:-63.7889415,
+        },
+    },
+    {
+        content :"Little Lighthouse",
+        coords:{
+            lat:46.3909226,
+            lng:-63.7889259,
+        },
+    },
+    {
+        content :"Trash Can",
+        coords:{
+            lat:46.3910552,
+            lng:-63.7884373,
+        },
+    },
+    {
+        content :"Wyatt Historic House",
+        coords:{
+            lat:46.3943908,
+            lng:-63.7889807,
+        },
+    },
+    {
+        content :"Lefurgey Building",
+        coords:{
+            lat:46.3942743,
+            lng:-63.7880634,
+        },
+    },
+    {
+        content :"Pillory",
+        coords:{
+            lat:46.3891329,
+            lng:-63.7874286,
+        },
+    },
+    {
+        content :"Lighthouse",
+        coords:{
+            lat:46.3889165,
+            lng:-63.7863799,
+        },
+    },
+    {
+        content :"Batting Cage",
+        coords:{
+            lat:46.3988341,
+            lng:-63.7869126,
+        },
+    },
+    {
+        content :"Concession Stand",
+        coords:{
+            lat:46.3989802,
+            lng:63.7875228,
+        },
+    },
+    {
+        content :"Bench",
+        coords:{
+            lat:46.3893834,
+            lng:-63.7854358,
+        },
+    },
+    {
+        content :"Leger Park",
+        coords:{
+            lat:46.4036024,
+            lng:-63.7886664,
+        },
+    },
+    {
+        content :"Piper Statue",
+        coords:{
+            lat:46.3944505,
+            lng:-63.768952,
+        },
+    },
+
+]
+
+export default markers;
